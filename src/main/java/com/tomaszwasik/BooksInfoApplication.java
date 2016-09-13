c819cff18a7ce917dc3e8780d07db54dca404462
tomaszwasik <tomaszwasik@live.com>
2016-09-13 22:21:25 +0200

    Initialization commit

---
+++ src/main/java/com/tomaszwasik/BooksInfoApplication.java
@@ -0,0 +1,12 @@
+package com.tomaszwasik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksInfoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tomaszwasik/BooksInfoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tomaszwasik;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BooksInfoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
